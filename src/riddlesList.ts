a6ad470838d97eca9e54fa25f58fffb0636d1625
Santi Andrade <sandrade@decentraland.org>
2025-04-24 01:36:15 +0200

    Add more doors

--- src/riddlesList.ts
+++ src/riddlesList.ts
@@ -8,5 +8,40 @@ export const riddlesList = [
         id: 1,
         riddleQuestion: 'I always move forward but never backward. What am I?',
         riddleAnswer: 'Time',
-    }
+    },
+    {
+        id: 2,
+        riddleQuestion: 'I’m not alive, but I grow. I don’t have lungs, but I need air. If I drink, I die. What am I?',
+        riddleAnswer: 'Fire',
+    },
+    {
+        id: 3,
+        riddleQuestion: 'I’m always in front of you, but you can never see me. What am I?',
+        riddleAnswer: 'Future',
+    },
+    {
+        id: 4,
+        riddleQuestion: 'I can fill a room but take up no space. What am I?',
+        riddleAnswer: 'Light',
+    },
+    {
+        id: 5,
+        riddleQuestion: 'The more of me there is, the less you see. What am I?',
+        riddleAnswer: 'Darkness',
+    },
+    {
+        id: 6,
+        riddleQuestion: 'I have keys but no locks. I have space but no room. You can enter but not go outside. What am I?',
+        riddleAnswer: 'Keyboard',
+    },
+    {
+        id: 7,
+        riddleQuestion: 'I swing in trees, I love bananas. What am I?',
+        riddleAnswer: 'Monkey',
+    },
+    {
+        id: 8,
+        riddleQuestion: 'I’m a digital world, vast and diverse. What am I?',
+        riddleAnswer: 'Decentraland',
+    },
 ];
